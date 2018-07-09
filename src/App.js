a50ce51f79a502a1438973b6c7801997e4d49cd1
andrea.duque <andrea.duque@pe.senai.br>
2018-07-09 15:48:55 -0300

    change tags according to time

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
 //Vai receber o valor da descricao e atualizar o bagulho
 //componente aparece e some -> criar algo pra ele saber quando aparecer e sumir
 class Tag extends Component {
@@ -11,7 +12,7 @@ class Tag extends Component {
             <div className="card text-white bg-primary mb-3" style={{ maxWidth: 10 + 'em', maxHeight: 10 + 'em' }}>
                 {/* <div className="card-header">Produto 1</div> */}
                 <div className="card-body">
-                    <h5 className="card-title">Foo</h5>
+                    <h5 className="card-title">{this.props.description}</h5>
                     <p className="card-text">foo foo foo foo</p>
                 </div>
             </div>
@@ -29,48 +30,58 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentTagIndex: null,            
+            currentTagIndex: null,
+            previousTagIndex: null,
+            currentTagDescription: ""
         };
         this.videoRef = React.createRef();
-        this.tagTimes = [2,6];
-        this.tagDescriptions = ["blahblah foo", "blahblah foo 2"];
+        this.tagTimes = [2, 6];
+        this.tagDescriptions = ["Tag 1", "Tag 2"];
     }
 
     handleTimeUpdate() {
-        const tag = this.state.currentTagIndex;
+        const currentTag = this.state.currentTagIndex;
+        const previousTag = this.state.previousTagIndex;
+        //const currentTagDescription = this.state.currentTagDescription;
+
         let currentTime = Math.floor(this.videoRef.current.currentTime);
         let tagIndex = this.tagTimes.indexOf(currentTime);
-        
-        if(tagIndex > -1 && tagIndex !== tag){
+
+        if (tagIndex > -1 && tagIndex !== previousTag) {
             console.log("APARECE TAG");
             console.log(this.tagTimes[tagIndex]);
             console.log(this.tagDescriptions[tagIndex]);
-            
-            // this.setState({
-            //     currentTagIndex: tagIndex
-            // });
+
+            this.setState({
+                currentTagIndex: tagIndex,
+                previousTagIndex: currentTag,
+                currentTagDescription: this.tagDescriptions[tagIndex]
+            });
+
+            console.log(this.state)
         }
-
-        this.setState({
-            currentTagIndex: tagIndex
-        });
-
         console.log(tagIndex)
     }
 
     renderTag() {
-        return <Tag />;
+        return <Tag description = {this.state.currentTagDescription} />;
     }
 
     render() {
         return (
-            <div>
-                <div className="video">
-                    <video onTimeUpdate={() => this.handleTimeUpdate()} ref={this.videoRef} height="50%" width="50%" id="video" controls>
-                        <source src="http://www.w3schools.com/html/mov_bbb.mp4"></source>
-                    </video>
-                </div>
-                <div className="tag">
+            <div style={{ width: "640px", height: "360px" }}>
+                <video
+                    onTimeUpdate={() => this.handleTimeUpdate()}
+                    className='p-absolute'
+                    height="100%"
+                    width="100%"
+                    id="video"
+                    controls={true}
+                    ref={this.videoRef}
+                >
+                    <source src="http://www.w3schools.com/html/mov_bbb.mp4"></source>
+                </video>
+                <div className="p-absolute">
                     {this.renderTag()}
                 </div>
             </div>
